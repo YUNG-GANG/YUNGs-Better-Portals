5a93cf53a3ee2f8689fe0bac8c04fd8a30f298ba
Nicholas Young <ndy14@ufl.edu>
2020-11-23 22:14:52 -0500

    Add separate creative tab. Make reclaimer block drop item. Fix reclaimer item model to be scaled properly (temporary).

--- src/main/java/com/yungnickyoung/minecraft/betterportals/item/ItemModule.java
+++ src/main/java/com/yungnickyoung/minecraft/betterportals/item/ItemModule.java
@@ -1,20 +1,34 @@
 package com.yungnickyoung.minecraft.betterportals.item;
 
+import com.yungnickyoung.minecraft.betterportals.BetterPortals;
 import com.yungnickyoung.minecraft.betterportals.block.BlockModule;
 import com.yungnickyoung.minecraft.betterportals.config.BPSettings;
 import com.yungnickyoung.minecraft.betterportals.fluid.FluidModule;
 import com.yungnickyoung.minecraft.betterportals.module.IModule;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class ItemModule implements IModule {
+    /**
+     * Creative tab
+     */
+    public static final ItemGroup BETTERPORTALS_CREATIVE_TAB = new ItemGroup(ItemGroup.GROUPS.length, BPSettings.MOD_ID) {
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public ItemStack createIcon() {
+            return new ItemStack(BlockModule.RECLAIMER_BLOCK);
+        }
+    };
+
     public static Item PORTAL_BUCKET = new BucketItem (
         () -> FluidModule.PORTAL_FLUID,
-        new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC)
+        new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(BETTERPORTALS_CREATIVE_TAB)
     );
-    public static Item RECLAIMER = new BlockItem(BlockModule.RECLAIMER_BLOCK, new Item.Properties().group(ItemGroup.MISC));
+    public static Item RECLAIMER = new BlockItem(BlockModule.RECLAIMER_BLOCK, new Item.Properties().group(BETTERPORTALS_CREATIVE_TAB));
 
     public void init() {
         FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Item.class, ItemModule::registerItems);
