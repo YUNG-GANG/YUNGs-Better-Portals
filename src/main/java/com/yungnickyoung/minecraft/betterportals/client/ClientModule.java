ac618fc6e5d77de8bc47f7dbfaedc245b14c81b2
Nicholas Young <ndy14@ufl.edu>
2021-01-21 22:47:07 -0500

    Remove unused method for changing color of bucket icon.

--- src/main/java/com/yungnickyoung/minecraft/betterportals/client/ClientModule.java
+++ src/main/java/com/yungnickyoung/minecraft/betterportals/client/ClientModule.java
@@ -21,25 +21,12 @@ public class ClientModule implements IModule {
 
     private static void registerClientListeners() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientModule::onClientSetup);
-//        FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientModule::addColorHandlers);
         MinecraftForge.EVENT_BUS.addListener(OverlayRenderer::renderUnderwaterOverlay);
         MinecraftForge.EVENT_BUS.addListener(OverlayRenderer::renderPortalOverlay);
         MinecraftForge.EVENT_BUS.addListener(OverlayRenderer::renderReclaimerOverlay);
 //        MinecraftForge.EVENT_BUS.addListener(PortalPerturbRenderer::renderPortalPerturb);
         MinecraftForge.EVENT_BUS.addListener(OverlayRenderer::renderDebugOverlay);
     }
-
-    /**
-     * Adds color mask to the dimensional plasma fluid bucket item.
-     */
-//    private static void addColorHandlers(ColorHandlerEvent.Item event) {
-//        event.getItemColors().register(
-//            (stack, index) -> FluidUtil.getFluidContained(stack)
-//                .map(fstack -> fstack.getFluid().getAttributes().getColor(fstack))
-//                .orElse(0xFFFFFFFF),
-//            ItemModule.PORTAL_BUCKET
-//        );
-//    }
 
     /**
      * Binds TE renderer(s) and adds necessary render layers for portal fluid and reclaimer blocks.
